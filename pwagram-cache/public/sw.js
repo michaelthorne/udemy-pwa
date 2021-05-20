414c9e307bb7b3753a377dfc978d566fc7e8351e
Michael Thorne <michael.thorne@gmail.com>
2021-05-20 09:18:38 +0200

    Caching with request URL parsing

--- pwagram-cache/public/sw.js
+++ pwagram-cache/public/sw.js
@@ -1,26 +1,28 @@
 const CACHE_STATIC_NAME = 'static-v0.0.5'
 const CACHE_DYNAMIC_NAME = 'dynamic-v0.0.1'
 
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+]
+
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker …', event)
   // Use the Cache API to cache the static app shell.
   // N.B. Wait until the cache preparation is complete before continuing!
   event.waitUntil(caches.open(CACHE_STATIC_NAME).then((cache) => {
     console.log('[Service Worker] Precaching App Shell …', cache)
-    cache.addAll([
-        '/',
-        '/index.html',
-        '/offline.html',
-        '/src/js/app.js',
-        '/src/js/feed.js',
-        '/src/js/material.min.js',
-        '/src/css/app.css',
-        '/src/css/feed.css',
-        '/src/images/main-image.jpg',
-        'https://fonts.googleapis.com/css?family=Roboto:400,700',
-        'https://fonts.googleapis.com/icon?family=Material+Icons',
-        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-    ])
+    cache.addAll(STATIC_ASSETS)
     // cache.add('/')
     // cache.add('/index.html')
     // cache.add('/src/js/app.js')
@@ -57,6 +59,11 @@ self.addEventListener('fetch', (event) => {
           })
         })
     )
+  } else if (new RegExp('\\b' + STATIC_ASSETS.join('\\b|\\b') + '\\b').test(event.request.url)) { // Check if request URL is for any of the static assets
+    // Cache only strategy
+    event.respondWith(
+        caches.match(event.request)
+    )
   } else {
     // Cache, with network fallback strategy.
     event.respondWith(
@@ -74,7 +81,10 @@ self.addEventListener('fetch', (event) => {
           }).catch((err) => {
             // Return an offline page if there is no item found in the cache
             return caches.open(CACHE_STATIC_NAME).then((cache) => {
-              return cache.match('/offline.html');
+              if (event.request.url.indexOf('/help') > -1) {
+                // Only show offline when attempting to access the help page
+                return cache.match('/offline.html');
+              }
             })
           })
         })
